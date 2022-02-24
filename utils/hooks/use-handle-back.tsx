26545e9fae4e5f2981130e179591bf020903d6b2
lockedNLevered <shaynel@sfu.ca>
2022-02-23 19:41:28 -0800

    fix bug when deleting more items than exist from rows beyond one

--- utils/hooks/use-handle-back.tsx
+++ utils/hooks/use-handle-back.tsx
@@ -5,14 +5,18 @@ export default function useHandleBack() {
     const { target, board, setBoard, setTarget, attempt } = useContext(GameContext);
     return function (e: React.SyntheticEvent<HTMLButtonElement>) {
         e.preventDefault();
-        if (target > 0) {
-            const newTarget = (oldTarget: number) => oldTarget - 1 >= 0 ? oldTarget - 1 : 0;
+        //Minimum value you can delete to is: 5 * the attempt number + 1 (attempt is zero indexed) - 5. 
+        //Otherwise you will delete into previous attempt
+        const floor = ((5 * (attempt + 1)) - 5);
+        if (target > floor) {
+            const newTarget = (oldTarget: number) => oldTarget - 1 >= floor ? oldTarget - 1 : floor;
             const resetGuess = {
                 id: newTarget(target),
                 value: "",
                 correct: false,
                 wrongIndex: false
             };
+
             board[attempt].splice(newTarget(target) % 5, 1, resetGuess);
             const newBoard = board.slice();
             setBoard(newBoard);
