1fe6bf8d22d5ed463b691be1377470a19b1dc031
lockedNLevered <shaynel@sfu.ca>
2022-02-24 17:46:10 -0800

    temporarily remove sign in option

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { signIn, useSession } from "next-auth/react";
 import ResetButton from "../ResetButton/ResetButton";
 import styles from './Nav.module.scss';
-import ProfileButton from "../ProfileButton/ProfileButton";
+
 export default function Nav() {
-    const { data: session } = useSession();
+  
   
     return (
         <header className={styles["nav"]}>
             <h1>Chai Time 🍵</h1>
             <div className={styles["nav__options"]}>
-                {!session && <button className={styles["nav__sign-in"]} onClick={() => signIn("github")}>Sign In</button>}
-                {session && <ProfileButton />}
+                {/* {!session && <button className={styles["nav__sign-in"]} onClick={() => signIn("github")}>Sign In</button>}
+                {session && <ProfileButton />} */}
                 <ResetButton />
             </div>
 
